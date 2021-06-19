888c1575f2930e788363b24a1f34813139ab8862
Pedro <theskorpions2014@gmail.com>
2021-06-20 00:01:19 +0300

    Added webhooks

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
+import logging
+
 from aiogram import types, filters
 from aiogram.utils import executor
+from aiogram.utils.executor import start_webhook
 
 from dispatcher import dp, bot
 from fixer_currency.fixer_currency import lst, convert, history
-
+from config import WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_PORT, WEBAPP_HOST
 
 @dp.message_handler(filters.RegexpCommandsFilter(regexp_commands=['exchange ([0-9]*) ([\w]*) to ([\w]*)']))
 async def exchange(message: types.Message, regexp_command):
@@ -23,5 +26,27 @@ async def stats(message: types.Message, regexp_command):
     await bot.send_photo(message.from_user.id, open(response, 'rb'), f'График за последние {regexp_command.group(3)} дней')
 
 
+@dp.message_handler(commands='start', command_prefix='/')
+async def welcome(message: types.Message):
+    await message.answer('Im working')
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    logging.info(dp)
+
+
+async def on_shutdown(dp):
+    logging.info(dp)
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
